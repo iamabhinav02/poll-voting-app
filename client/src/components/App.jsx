434b8cacb84e90ddb0004729e4a00a2fde411b1b
Abhinav Kumar <iamabhinav02@gmail.com>
2020-12-06 12:59:29 +0530

    Set up client API service

---
+++ client/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import api from '../services/api';
+
+class App extends Component {
+    async componentDidMount() {
+        const result = await api.call('post', `auth/login`, {
+            username: "abhinav",
+            password: "password"
+        });
+        console.log(result);
+    }
+    render() {
+        return <div>App works</div>;
+    }
+}
+
+export default App;
