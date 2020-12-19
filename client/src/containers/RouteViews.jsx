0e9d40962a665392cb3e8f9be50d07427ac59d3e
Abhinav Kumar <iamabhinav02@gmail.com>
2020-12-20 01:42:38 +0530

    Finished building the logic part of the application

--- client/src/containers/RouteViews.jsx
+++ client/src/containers/RouteViews.jsx
@@ -8,11 +8,19 @@ import AuthPage from "../pages/AuthPage";
 import HomePage from "../pages/HomePage";
 import TestPage from "../pages/TestPage";
 import PollPage from "../pages/PollPage";
+import CreatePollPage from "../pages/CreatePollPage";
 
 const RouteViews = ({ auth, getCurrentPoll }) => (
 	<main>
 		<Switch>
 			<Route exact path="/" render={props => <HomePage {...props} />} />
+			<Route
+				exact
+				path="/poll/new"
+				render={() => (
+					<CreatePollPage isAuthenticated={auth.isAuthenticated} />
+				)}
+			/>
 			<Route
 				exact
 				path="/login"
